4a846c4b9c1d851f62012772fa060e8de8f58b76
Alisha <alishakamat8@gmail.com>
2022-08-10 01:48:23 +0530

    Feature Enhancements for Paragraph Structure

--- layouts/joomla/edit/paragraphstructure.php
+++ layouts/joomla/edit/paragraphstructure.php
@@ -23,11 +23,44 @@ $total_paragraphs = preg_match_all("/<p.*?>(.*?)<\/p>/is", $form->getValue('arti
 
 $paragraph = 0;
 $paragraph_displayed = 0;
+$long_paragraphs = 0;
+$paragraph_list = array();
 
-//Displays the first 5 ill-formed paragraphs
-while($paragraph < $total_paragraphs && $paragraph_displayed < 5)
+while($paragraph < $total_paragraphs)
 {
 	if(str_word_count(strip_tags($matches[0][$paragraph])) > $max_length)
+    {
+        $paragraph_list[$long_paragraphs] = strip_tags($matches[0][$paragraph]);
+        $long_paragraphs++;
+    }
+    $paragraph++;
+}
+
+for($i=0;$i<$long_paragraphs-1;$i++)
+{
+    for($j=0;$j<$long_paragraphs-$i-1;$j++)
+    {
+        if(str_word_count($paragraph_list[$j]) < str_word_count($paragraph_list[$j+1]))
+        {
+            $temp = $paragraph_list[$j];
+            $paragraph_list[$j] = $paragraph_list[$j+1];
+            $paragraph_list[$j+1] = $temp;
+        }
+    }
+}
+
+$paragraph = 0;
+
+if($long_paragraphs == 0)
+{
+    $none_text = str_replace('{max_length}', $max_length, Text::_('COM_CONTENT_FIELD_PARAGRAPH_STRUCTURE_NONE'));
+    echo "<i>" . $none_text . "</i>";
+}
+else
+{
+    echo $long_paragraphs . " discovered out of total " . $total_paragraphs . "<br>";
+
+    while($paragraph < $long_paragraphs && $paragraph_displayed < 5)
     {
         if($paragraph_displayed == 0)
         {
@@ -35,22 +68,13 @@ while($paragraph < $total_paragraphs && $paragraph_displayed < 5)
             echo "<ul>";
         }
         echo "<li>";
-        echo "<i>" . substr(strip_tags($matches[0][$paragraph]), 0, 30) . "...</i>\n";
-        echo "[" . str_word_count(strip_tags($matches[0][$paragraph])) . " " . Text::_('COM_CONTENT_FIELD_WORDS_DESC') . "]";// . ($matches[0][$i]);
+        echo "<i>" . substr($paragraph_list[$paragraph_displayed], 0, 30) . "...</i>\n";
+        echo "[" . str_word_count($paragraph_list[$paragraph_displayed]) . " " . Text::_('COM_CONTENT_FIELD_WORDS_DESC') . "]";// . ($matches[0][$i]);
         echo "</li>";
         $paragraph_displayed++;
+        $paragraph++;
     }
-    $paragraph++;
+    echo "</ul>";
 }
-echo "</ul>";
-
-
-if($paragraph_displayed == 0)
-{
-    $none_text = str_replace('{max_length}', $max_length, Text::_('COM_CONTENT_FIELD_PARAGRAPH_STRUCTURE_NONE'));
-    echo "<i>" . $none_text . "</i>";
-}
-
-
 
 ?>
